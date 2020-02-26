7727ed1497b531bc34ee8a01769d14916ebb4199
wangyanzhao <wyz159753@gmail.com>
2020-02-26 20:41:11 +0800

    modify start

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 PID_FILE=server.pid
 
-./server -log_dir=. -log_level=info -id $ID -algorithm $1 -fz=0 &
+nohup ./server -log_dir=. -log_level=info -id $ID -algorithm $1 -fz=0 &
 echo $! >> ${PID_FILE}
