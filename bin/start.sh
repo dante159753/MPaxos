b808cff500493da03ec8156a141add3ef62e1553
wangyanzhao <wyz159753@gmail.com>
2020-02-26 21:00:42 +0800

    Revert "modify start"
    This reverts commit 7727ed1497b531bc34ee8a01769d14916ebb4199.

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 PID_FILE=server.pid
 
-nohup ./server -log_dir=. -log_level=info -id $ID -algorithm $1 -fz=0 &
+./server -log_dir=. -log_level=info -id $ID -algorithm $1 -fz=0 &
 echo $! >> ${PID_FILE}
